fbbd98d504af450226db4aada3744ab7723e820d
Sese <sese.miguel9@gmail.com>
2020-06-30 23:10:03 +0800

    initial commit

---
+++ src/components/MainContent.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+function MainContent() {
+    return (
+        <div>
+            <main>
+                <p>My Name is Miguel Karlo Sese</p>
+                <p>I am an Associate Consultant</p>
+                <p>Skills:</p>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>React.JS</li>
+                </ul>
+            </main>
+
+        </div>
+    )
+}
+
+export default MainContent;
